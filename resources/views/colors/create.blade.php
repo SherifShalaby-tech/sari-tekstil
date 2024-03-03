71845174914b28d2b067a6d1545366765e20860b
nour-alshikh <nooralshikh30@gmail.com>
2024-03-03 13:58:36 +0200

    first commit

--- resources/views/colors/create.blade.php
+++ resources/views/colors/create.blade.php
@@ -1,21 +1,20 @@
 <!-- Modal -->
-<div class="modal fade" id="createColorModal" tabindex="-1" role="dialog" aria-labelledby="exampleStandardModalLabel" style="display: none;" aria-hidden="true">
+<div class="modal fade" id="createColorModal" tabindex="-1" role="dialog" aria-labelledby="exampleStandardModalLabel"
+    style="display: none;" aria-hidden="true">
     <div class="modal-dialog  rollIn  animated" role="document">
         <div class="modal-content">
             <div class="modal-header">
-                <h5 class="modal-title" id="exampleStandardModalLabel">{{__('lang.add')}}</h5>
+                <h5 class="modal-title" id="exampleStandardModalLabel">{{ __('lang.add') }}</h5>
                 <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                <span aria-hidden="true">×</span>
+                    <span aria-hidden="true">×</span>
                 </button>
             </div>
-            {!! Form::open(['route' => 'colors.store', 'method' => 'post', 'files' => true,'id' =>'color-form' ]) !!}
+            {!! Form::open(['route' => 'colors.store', 'method' => 'post', 'files' => true, 'id' => 'color-form']) !!}
             <div class="modal-body">
                 <div class="form-group">
                     {{-- <input type="hidden" name="quick_add" value="{{ isset($quick_add)&&$quick_add?$quick_add:'' }}"> --}}
-                    {!! Form::label('name', __( 'lang.name' ) . ':*') !!}
-                    {!! Form::text('name', null, ['class' => 'form-control', 'placeholder' => __( 'lang.name' ), 'required'
-                    ]);
-                    !!}
+                    {!! Form::label('name', __('lang.name') . ':*') !!}
+                    {!! Form::text('name', null, ['class' => 'form-control', 'placeholder' => __('lang.name'), 'required']) !!}
                     @error('name')
                         <label class="text-danger error-msg">{{ $message }}</label>
                     @enderror
@@ -23,12 +22,13 @@
             </div>
             <div class="modal-footer">
                 <button type="button" class="btn btn-secondary" data-dismiss="modal">@lang('lang.close')</button>
-                <button  id="create-nationality-btn" class="btn btn-primary">{{__('lang.save')}}</button>
+                <button id="create-nationality-btn" class="btn btn-primary">{{ __('lang.save') }}</button>
             </div>
             {!! Form::close() !!}
+            {{--  --}}
         </div>
     </div>
 </div>
 
-{!! JsValidator::formRequest('App\Http\Requests\ColorRequest','#color-form'); !!}
+{!! JsValidator::formRequest('App\Http\Requests\ColorRequest', '#color-form') !!}
 {{-- {!! JsValidator::formRequest('App\Http\Requests\NationalityRequest','#quick_add_nationality_form'); !!} --}}
